c7c0d4f3fb234dde06518d9531aecef96ddc7b52
hiien29 <hdtn792@gmail.com>
2023-06-25 15:42:04 +0900

    tailwindow

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -1,9 +1,9 @@
 <x-guest-layout>
 
-    <div class="backtop">
+    <div class="text-center">
         <a href="{{ route('admin.login') }}">管理者の方はこちら</a>
     </div>
-    <div class="backtop" style="margin-bottom: 30px">
+    <div class="text-center mt-10">
         <a href="{{ route("login") }}">従業員の方はこちら</a>
     </div>
         
